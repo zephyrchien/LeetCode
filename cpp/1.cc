7d620ca93bdd4f7cd86505197efc4e57a874535b
zephyr <i@zephyr.moe>
2021-05-14 23:28:23 +0900

    1_2

--- cpp/1.cc
+++ cpp/1.cc
@@ -7,23 +7,46 @@ class Solution
 {
 	public:
 	    vector<int> twoSum(vector<int>& nums, int target) 
-	{
-        int len = nums.size();
-        std::vector<int> vec(2);
-        for (int i=0; i<len; i++)
-        {
-            for (int j=i+1; j<len; j++)
-            {
-                if (nums[i]+nums[j] == target)
-                {
-                    vec[0] = i;
-                    vec[1] = j;
-                    break;
-                }
-            }
-        }
-        return vec;
-    }
+		{
+			int len = nums.size();
+			std::vector<int> vec(2);
+			for (int i=0; i<len; i++)
+			{
+				for (int j=i+1; j<len; j++)
+				{
+				    if (nums[i]+nums[j] == target)
+				    {
+				        vec[0] = i;
+				        vec[1] = j;
+				        break;
+				    }
+				}
+			}
+		    return vec;
+    	}
+    
+    	vector<int> twoSum_2(vector<int>& nums, int target)
+    	{
+		    int len = nums.size();
+		    std::vector<int> res(2);
+		    std::unordered_map<int,int> map;
+
+		    for (int i=0; i<len; i++)
+		    {
+		        map[nums[i]] = i;
+		    }
+		    auto not_found = map.end();
+		    for (int i=0; i<len; i++)
+		    {
+		        auto found = map.find(target - nums[i]);
+		        if (found != not_found && found->second != i)
+		        {
+		            res[0] = i;
+		            res[1] = found->second;
+		        }
+		    }
+        return res;
+    	}
 };
 
 int main(int argc, char** argv)
